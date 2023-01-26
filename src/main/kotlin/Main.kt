c981d7232edc9afe20722ba3f481c472779b585c
onion-debugger <oshiole94@gmail.com>
2023-01-26 23:10:55 +0100

    Learning about numbers

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,33 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    // Variables
+    val aValue : Int = 10
+    var aVariable : String = "ABC"
+    println(aValue)
+    println(aVariable)
+
+    aVariable =  "CDE"
+    println(aVariable)
+
+    // Number Type
+    val integerValue = 42
+    val doubleValue = 3.14
+    val longLiteral = 1_000_000L
+    val float = 123.45F
+
+    println(float)
+    println(doubleValue)
+
+    // Type conversion
+    val l2 = integerValue.toLong()
+    println(longLiteral)
+    println(l2)
+
+    println()
+
+    // other numeral systems
+    val hexBytes = 0xA4_D6_FE_FE
+    println(hexBytes)
+    val bytes = 0b01010010_01101101_11101000_10010010 // binary numeric system
+    println(bytes)
 }
---
+++ src/main/kotlin/numbers.kt
@@ -0,0 +1,44 @@
+fun main() {
+    val i1 = 12
+    val i2 = 34
+
+    // Operations on numbers
+    println(i1 + i2)
+    println(i1 - i2)
+    println(i1 * i2)
+    println(i1 / i2)
+
+    // Doubles
+    val d1 = 1.4
+    val d2 = 2.5
+
+    println(d1 + d2)
+    println(d1 - d2)
+    println(d1 * d2)
+    println(d1 / d2)
+
+    println()
+
+    println(5.toDouble() / 2)
+
+    println()
+
+    // Remainder operator {%}
+    println(1 % 3)
+    println(2 % 3)
+    println(3 % 3)
+    println(4 % 3)
+    println(5 % 3)
+    println(6 % 3)
+    println(7 % 3)
+    println(0 % 3)
+    println(-1 % 3)
+    println(-2 % 3)
+    println(-3 % 3)
+
+    println()
+
+    println(0b011 and 0b001)
+    println(0b011 or 0b001)
+    println(0b011 xor 0b001)
+}
